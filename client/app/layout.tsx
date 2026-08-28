616b598633d9a8ec8e47de8b920f7eac51dfa5fc
Tahsin005 <tahsin.ferdous3546@gmail.com>
2026-08-28 14:41:36 +0600

    feat(client): init nextjs app, add tanstack query, zod, zustand, shadcn ui as deps

---
+++ client/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/components/providers/query-provider";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "EduPulse - Modern Learning Management System",
+  description: "Accelerate your career with interactive courses, real-time analytics, and expert mentorship.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col bg-background text-foreground">
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
+
